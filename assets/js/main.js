66e5f5757edce1fed787d54dc21bb545720a1ca7
Martina Tarantino <martinatarantino02@gmail.com>
2025-09-28 15:21:00 +0200

    adding skills section and fixing some bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 /* Codice js dell'intera pagina */
+
+/* CREATING CARDS FOR SKILLS' SECTION */
+const $skillsContainer = document.querySelector("div.skills-cards-container");
+
+let skillsCards = [
+  { name: "HTML", icon: "html" },
+  { name: "CSS", icon: "css" },
+  { name: "JS", icon: "js" },
+  { name: "TS", icon: "ts" },
+  { name: "React", icon: "react" },
+  { name: "Git", icon: "git" },
+  { name: "Figma", icon: "figma" },
+  { name: "AGILE", icon: "agile" },
+];
+
+skillsCards.forEach((skill) => {
+  let newCard = document.createElement("div");
+  newCard.innerHTML = `<img src='../assets/img/skills-icon/${skill.icon}-icon.svg' alt='iconsa ${skill.name}' title='iconsa ${skill.name}' /><h3>${skill.name}</h3>`;
+
+  newCard.classList.add("skill-card");
+
+  $skillsContainer.appendChild(newCard);
+});
